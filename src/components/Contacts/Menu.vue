3ffed72b8220a2af3ed9520e858137117ab05979
Endi <rokerlive50@gmail.com>
2025-02-04 14:55:52 +0300

    text

--- src/components/Contacts/Menu.vue
+++ src/components/Contacts/Menu.vue
@@ -29,7 +29,7 @@
         <a href="#" class="menu__link"> Price </a>
       </router-link>
       <li class="menu__item">
-        <a href="#working" class="menu__link"> Our works </a>
+        <a href="#" class="menu__link"> Our works </a>
       </li>
       <router-link to="/contacts" class="menu__item">
         <a href="#" class="menu__link active"> Contacts </a>
